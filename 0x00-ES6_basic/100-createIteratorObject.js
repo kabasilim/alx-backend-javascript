0f45651961291dfda31b9c6e52825e537d723bda
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-05 00:44:27 +0000

    all

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,28 @@
+export default function createIteratorObject(report) {
+  const employeesObj = report.allEmployees
+
+  employeesObj[Symbol.iterator] = () => {
+    let employeesByDept = Object.values(employeesObj);
+    let employeeIndex = 0;
+    let deptIndex = 0;
+
+    return {
+      next() {
+        if (employeeIndex >= employeesByDept[deptIndex].length) {
+          deptIndex++;
+          employeeIndex = 0;
+        }
+
+        if (deptIndex >= employeesByDept.length) {
+          return { value: undefined, done: true };
+        }
+
+        return {
+          value: employeesByDept[deptIndex][employeeIndex++],
+          done: false
+        }
+      }
+    }
+  }
+  return employeesObj;
+}
