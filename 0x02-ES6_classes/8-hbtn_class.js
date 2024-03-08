e63ef730c30170d1a021c73d291b63cc6a6ec055
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-08 01:19:32 +0000

    Primitive - Holberton Class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') throw new TypeError('size must be a number');
+    if (typeof location !== 'string') throw TypeError('location must be a string');
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
