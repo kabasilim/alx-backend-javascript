bd036b388944a5fa0879c07c411f3f8fa51b3016
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-13 01:42:47 +0000

    solution task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-	return [
-		{id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-		{id: 2, firstName: 'James', location: 'Columbia'},
-		{id: 5, firstName: 'Serena', location: 'San Francisco'}
-	];
+  const result = [];
+  result.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  result.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  result.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+  return result;
 }
