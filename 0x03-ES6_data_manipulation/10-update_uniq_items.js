ce59708bc84043fe47ac96368626d5a623ca82f6
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-13 02:19:56 +0000

    solution task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((item, key) => {
+    if (item === 1) {
+      map.set(key, 100);
+    }
+  });
+  return map;
+}
